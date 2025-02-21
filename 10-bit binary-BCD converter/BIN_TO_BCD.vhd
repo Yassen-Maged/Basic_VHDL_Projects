0e049348a1cb9b5a9c72cb655e8fe052e0148afc
Yassen Maged <139732789+Yassen-Maged@users.noreply.github.com>
2025-02-21 15:29:32 +0200

    Update BIN_TO_BCD.vhd
    Made it more generic.
    More comments has been added.

--- 10-bit binary-BCD converter/BIN_TO_BCD.vhd
+++ 10-bit binary-BCD converter/BIN_TO_BCD.vhd
@@ -2,20 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity BIN_TO_BCD is
-generic (n: natural:= 10);
-port(
-	SW: in std_logic_vector(9 downto 0); -- 10switches
-	led: out std_logic_vector(9 downto 0); -- 10 leds indicate which switches are enabled
-	HEX0,HEX1,HEX2,HEX3: out std_logic_vector(7 downto 0) -- 7-SEG displays
-	--HEX0: ones, HEX3: Thousands
+entity bin_to_bcd is
+generic(N: natural:= 10;
+BCD_N: natural :=13 -- (1 - 4 - 4 - 4) bits per display for 16-bit binary number
 );
-END BIN_TO_BCD;
+port(
+clk,reset: in std_logic;
+binary_in: in std_logic_vector(n-1 downto 0);
+HEX0,HEX1,HEX2,HEX3: out std_logic_vector(7 downto 0)
+);
+end bin_to_bcd;
 
-architecture behavioral of BIN_TO_BCD is
+architecture beh of bin_to_bcd is
 type segmap is array (0 to 9) of std_logic_vector (7 downto 0); -- binary to 7-seg map
-signal smap: segmap := --active low
-(
+constant smap: segmap := --active low
+( --Pgfedcba
 	"11000000", --0
 	"11111001", --1
 	"10100100", --2
@@ -25,36 +26,34 @@ signal smap: segmap := --active low
 	"10000010", --6
 	"11111000", --7
 	"10000000", --8
-	"10010000"  --9
+	"10010000"--9
 );
-signal num: unsigned(9 downto 0); --converting the std_logic_vector to unsigned for arithmatic operations
-signal bcd_reg : unsigned(12 downto 0) := (others => '0'); -- (1 - 4 - 4 - 4) bit per display
-							   -- (11111111111)2 => 1024
+signal num: unsigned(n-1 downto 0); --converting the std_logic_vector to unsigned for arithmatic operations
+signal bcd_reg : unsigned(BCD_N -1 downto 0) := (others => '0'); 
 begin
-led <= sw;
-num <= unsigned(sw);
+num <= unsigned(binary_in);
 
-process (num) --we don't need clock here
-variable bcd : unsigned(12 downto 0) := (others => '0');
+process (num) --I made it async design, but if you want to make it sync,
+			  --you can make it sensitive to the rising_edge of the clock.
+variable bcd : unsigned(BCD_N-1 downto 0) := (others => '0');
 begin
 bcd := (others => '0');
-	for i in 0 to 9 loop -- we iterate 10 times because we have 10 bits input
+	for i in 0 to n-1 loop -- we iterate N times because we have N bits input
 		for j in 0 to 2 loop
 			if bcd(4*j + 3 downto 4 * j) > 4 then --check 4 bits by 4 bits
 				bcd(4*j + 3 downto 4 * j) := bcd(4*j + 3 downto 4 * j) + 3; -- add 3
 			end if;
 		end loop;
-		bcd := bcd(11 downto 0) & num(9 - i); -- shift left by 1 bit
+		bcd := bcd(BCD_N-2 downto 0) & num(n-1 - i); -- shift left by 1 bit
 	end loop;
 bcd_reg <= bcd;
 end process;
 
-process(bcd_reg)
+process(bcd_reg) --Display the BCD number on 7-SEG display
 begin
-hex0 <= smap(to_integer(bcd_reg(3 downto 0)));
-hex1 <= smap(to_integer(bcd_reg(7 downto 4)));
-hex2 <= smap(to_integer(bcd_reg(11 downto 8)));
-hex3 <= smap(to_integer(bcd_reg(12 downto 12)));
+hex0 <= smap(to_integer(bcd_reg(3 downto 0)));	--ones
+hex1 <= smap(to_integer(bcd_reg(7 downto 4)));	--tens
+hex2 <= smap(to_integer(bcd_reg(11 downto 8)));	--hundreds
+hex3 <= smap(to_integer(bcd_reg(12 downto 12)));--thousands
 end process;
-
-end behavioral;
+end beh;
